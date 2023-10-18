a3cfb343498660f090c59b3809bbb944f0a84fc3
Smriti Yadav <99757048+Smriti347@users.noreply.github.com>
2023-10-18 20:58:01 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechStar</h1>
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://youtube.com/smriti"  target="">Youtube</a>
+                <a href="https://instagram.com/374_miti"  target="">Instagram</a>
+                <a href="https://github.com/smriti374"  target="">Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
